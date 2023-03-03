743e8d65296e9f2e29307508a61513a850ed7140
Arekk77 <daszkowskiarek145@gmail.com>
2023-03-03 21:02:32 +0100

    dodanie basic home pages  z nawigacja i formularzem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Button } from "@mantine/core";
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div>
         <Button onClick={() => setCount(count + 1)}>Count: {count}</Button>
---
+++ frontend/src/pages/home/homePages.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionComponent from "../../components/baner/baner";
+import NavbarComponent from "../../components/navbar/navbar";
+
+const HomePages = () => {
+    return (
+        <>
+        <NavbarComponent />
+        <SectionComponent />
+        </>
+    )
+}
+
+export default HomePages
+
